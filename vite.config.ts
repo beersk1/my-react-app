c876bee54c4fe58a85fca421c7bceea69bb66998
kabeer <kabeer@gmail.com>
2025-06-03 19:06:22 +0530

    Added 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/my-react-app/",
+  base: "/my-react-app1/",
   server: {
     host: "::",
     port: 8080,
